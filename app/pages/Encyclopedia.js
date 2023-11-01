a3d4907ceeadcd62869a47d6d1943d8a1faa39ad
TheroneAlmadin <102451950+therone18@users.noreply.github.com>
2023-11-01 17:45:48 +0800

    Images and Static Data updates

--- app/pages/Encyclopedia.js
+++ app/pages/Encyclopedia.js
@@ -1,13 +1,73 @@
-import { View, Text } from "react-native"
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Button,
+  StyleSheet,
+  ScrollView,
+  ImageBackground,
+} from "react-native";
+import Header from "../components/Header";
+import { plants } from "../static/data";
+const Encyclopedia = ({ navigation }) => {
+  const handleEncyclopediaButtonPressed = () => {
+    navigation.navigate("Encyclopedia");
+  };
+  return (
+    <SafeAreaView style={styles.main}>
+      <Header onPressCallback={handleEncyclopediaButtonPressed} />
+      <Text style={styles.titletext}>Encyclopedia</Text>
 
-const Encyclopedia = ({navigation}) => {
-    return (
-        <View>
-            <Text>
-                Encyclopedia
-            </Text>
-        </View>
-    )
-}
+      <ScrollView>
+        {plants.map((item, index) => (
 
-export default Encyclopedia
+
+          <View style={styles.itemContainer}>
+            <Text style={styles.itemsubtitletext}>{item["plantName_Family"]} </Text>
+            <Text key={index}  style={styles.itemtitletext}>{item["plantName_Local"]}</Text>
+          </View>
+
+          
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    flexDirection: "column",
+    alignItmes: "center",
+    backgroundColor: "#F9EBC7",
+  },
+  
+  titletext: {
+    paddingHorizontal: 15,
+    paddingTop: 15,
+    color: "#783D19",
+    fontSize: 40,
+    fontWeight: "600",
+  },
+
+  itemtitletext: {
+    paddingHorizontal: 15,
+    color: "#783D19",
+    fontSize: 25,
+    fontWeight: "400",
+  },
+  itemsubtitletext: {
+    paddingHorizontal: 15,
+    color: "#783D19",
+    fontSize: 15,
+    fontWeight: "300",
+  },
+
+  itemContainer:{
+    flex: 1,
+    backgroundColor: "#C4661F",
+    marginVertical: 5
+  }
+});
+
+export default Encyclopedia;
