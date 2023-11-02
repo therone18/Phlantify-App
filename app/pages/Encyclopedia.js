09f95afdafa4a685b3e861d6af3a5f3088e16a0e
TheroneAlmadin <102451950+therone18@users.noreply.github.com>
2023-11-02 17:02:56 +0800

    Encyclopedia skeleton complete
    Search function most definitely, improve later

--- app/pages/Encyclopedia.js
+++ app/pages/Encyclopedia.js
@@ -6,28 +6,71 @@ import {
   StyleSheet,
   ScrollView,
   ImageBackground,
+  TouchableHighlight,
 } from "react-native";
 import Header from "../components/Header";
+import { imagePaths } from "../static/encylopediaImageSourcing";
+import { LinearGradient } from "expo-linear-gradient";
 import { plants } from "../static/data";
 const Encyclopedia = ({ navigation }) => {
   const handleEncyclopediaButtonPressed = () => {
     navigation.navigate("Encyclopedia");
   };
+
+  const handlePress = (plantID) => {
+    console.log(plantID);
+  };
+
+  function sortArrayByKey(array, key) {
+    return array.sort((a, b) => {
+        const nameA = a[key].toUpperCase();
+        const nameB = b[key].toUpperCase();
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+        
+        return 0;
+    });
+}
+
+  const sortedPlants = sortArrayByKey(plants, 'plantName_Local');
+
+
   return (
     <SafeAreaView style={styles.main}>
       <Header onPressCallback={handleEncyclopediaButtonPressed} />
       <Text style={styles.titletext}>Encyclopedia</Text>
 
       <ScrollView>
-        {plants.map((item, index) => (
+        {sortedPlants.map((item, index) => (
 
 
-          <View style={styles.itemContainer}>
-            <Text style={styles.itemsubtitletext}>{item["plantName_Family"]} </Text>
-            <Text key={index}  style={styles.itemtitletext}>{item["plantName_Local"]}</Text>
-          </View>
+          <TouchableHighlight underlayColor="#F9EBC7" onPress={() => handlePress(item["plantID"])}>
+          <ImageBackground
+            style={styles.itemContainer}
+            source={item["sourceImage"]}
+          >
+            <LinearGradient
+              colors={["#C4661FCC", "#00000000"]}
+              start={{ x: 0, y: 1 }}
+              end={{ x: 0, y: 0 }}
+              style={styles.gradient}
+            >
+              <Text style={styles.itemsubtitletext}>
+                {item["plantName_Family"]}{" "}
+              </Text>
+              <Text key={item["plantID"]} style={styles.itemtitletext}>
+                {item["plantName_Local"]}
+              </Text>
+            </LinearGradient>
+          </ImageBackground>
 
-          
+          </TouchableHighlight>
+
+
         ))}
       </ScrollView>
     </SafeAreaView>
@@ -41,7 +84,7 @@ const styles = StyleSheet.create({
     alignItmes: "center",
     backgroundColor: "#F9EBC7",
   },
-  
+
   titletext: {
     paddingHorizontal: 15,
     paddingTop: 15,
@@ -52,22 +95,30 @@ const styles = StyleSheet.create({
 
   itemtitletext: {
     paddingHorizontal: 15,
-    color: "#783D19",
+    color: "white",
     fontSize: 25,
-    fontWeight: "400",
+    fontWeight: "600",
+    paddingBottom:15
   },
   itemsubtitletext: {
     paddingHorizontal: 15,
-    color: "#783D19",
+    color: "white",
     fontSize: 15,
-    fontWeight: "300",
+    fontWeight: "400",
   },
 
-  itemContainer:{
+  itemContainer: {
     flex: 1,
-    backgroundColor: "#C4661F",
-    marginVertical: 5
-  }
+    marginVertical: 5,
+    paddingTop: 30,
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+  },
+  gradient: {
+    flex: 1,
+    height: "100%",
+    justifyContent: "flex-end", // Items at the bottom
+    alignItems: "flex-start", // Items at the left
+  },
 });
 
 export default Encyclopedia;
