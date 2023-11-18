777f4920af2fd947a75fdb7b00bd443bfe673b5a
TheroneAlmadin <102451950+therone18@users.noreply.github.com>
2023-11-18 16:31:28 +0800

    init

---
+++ app/pages/PlantDetails.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native"
+
+const PlantDetails = ({navigation}) => {
+    return (
+        <View>
+            <Text>
+                Plant Details
+            </Text>
+        </View>
+    )
+}
+
+export default PlantDetails
